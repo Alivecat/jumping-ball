dc3fefe6afbf8c3f146db0b2c97f9d21f4eb4b68
Alivecat <136169737@qq.com>
2017-09-09 22:50:06 +0800

    Import the game
    Import the game, something modified base on Brackeys/Color-Switch-Replica

---
+++ jumping-ball/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] circleGroup;
+    [Space]
+    public GameObject[] spawnPointGroup;
+    [Space]
+    public int randomIndex;
+
+    // Use this for initialization
+	void Start () {
+
+        RandomIndex();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void RandomIndex()
+    {
+        randomIndex = Random.Range(0, 2);
+    }
+
+    void spawnCircle()
+    {
+        foreach (GameObject point in spawnPointGroup)
+        {
+            Instantiate(circleGroup[randomIndex], point.transform.position, point.transform.rotation);
+        }
+        
+    }
+
+}
---
+++ jumping-ball/Assets/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed;
+
+
+    void Start()
+    {
+        RandomSpeed();
+    }
+
+	void Update () {
+		transform.Rotate(0f, 0f, speed * Time.deltaTime);
+	}
+
+    void RandomSpeed()
+    {
+        speed = Random.Range(50, 250);
+    }
+
+}
